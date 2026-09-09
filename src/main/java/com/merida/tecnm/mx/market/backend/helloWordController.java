ebf9ed6a601928272b974a5a3d5650cb7a6c2868
Isael Ojeda <isaelreyes427@gmail.com>
2026-09-08 18:12:48 -0600

    adding helloWord to test Spring configuration

--- src/main/java/com/merida/tecnm/mx/market/backend/helloWordController.java
+++ src/main/java/com/merida/tecnm/mx/market/backend/helloWordController.java
@@ -1,4 +1,13 @@
 package com.merida.tecnm.mx.market.backend;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/saludar")
 public class helloWordController {
+    @GetMapping("/saludo")
+    public String helloWord(){
+        return "hello Word!";
+    }
 }
